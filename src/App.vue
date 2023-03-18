de75460a25a40f0a7356486015d5332a71fc8785
OpenYong <fkszm3@gmail.com>
2023-03-18 12:47:17 +0900

    Vue Composition API Setup Method

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>{{ name }}</h1>
+  <button @click="placeOrder">주문</button>
+</template>
+
+<script>
+export default {
+  setup() {
+    const name = "The Snazzy Burger";
+    const placeOrder = () => {
+      alert("Your order has been placed!");
+    };
+    return { name, placeOrder };
+  },
+};
+</script>
+
+<style scoped></style>
