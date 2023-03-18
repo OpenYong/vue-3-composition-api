b061a7f47e8118bc3680a3f75f081bab9a5a846f
OpenYong <fkszm3@gmail.com>
2023-03-18 13:59:55 +0900

    Reactive Primitives with Refs

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,24 @@
 <template>
   <h1>{{ name }}</h1>
+  <input v-model="name" type="text" />
   <button @click="placeOrder">주문</button>
   <YummyMeal name="Bento 🍱" :price="9.99" @addToCart="addItemToCart" />
 </template>
 
 <script>
 import YummyMeal from "./components/YummyMeal.vue";
+import { ref } from "vue";
 
 export default {
   components: {
     YummyMeal,
   },
   setup() {
-    const name = "The Snazzy Burger";
+    const name = ref("The Snazzy Burger");
+
+    name.value = "바꿀 수 있다!";
+
+    // console.log(name);
 
     const placeOrder = () => {
       alert("Your order has been placed!");
